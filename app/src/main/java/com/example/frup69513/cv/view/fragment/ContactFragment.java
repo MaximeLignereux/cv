ce918093937a0d801f7278f6ee87c56824eef2a8
Max5210 <maxime.lignereux@gmail.com>
2017-04-26 10:44:42 +0200

    Call function in ContactFragment

--- app/src/main/java/com/example/frup69513/cv/view/fragment/ContactFragment.java
+++ app/src/main/java/com/example/frup69513/cv/view/fragment/ContactFragment.java
@@ -1,9 +1,18 @@
 package com.example.frup69513.cv.view.fragment;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -14,13 +23,14 @@ import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
 import com.example.frup69513.cv.R;
+import com.example.frup69513.cv.model.Contact;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class ContactFragment extends Fragment {
+public class ContactFragment extends Fragment implements ActivityCompat.OnRequestPermissionsResultCallback {
 
     private final static String TAG = "ContactFragment";
 
@@ -32,6 +42,9 @@ public class ContactFragment extends Fragment {
     private DatabaseReference mDatabase;
 
     private Context mContext;
+
+    private static final int CALL_PERMISSION = 1;
+    private String callNumber;
 
     public ContactFragment() {
         // Required empty public constructor
@@ -102,7 +115,58 @@ public class ContactFragment extends Fragment {
         mDatabase.child(getReference()).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.d(TAG, "onCreateView():onDataChange():datasnaphot: " + dataSnapshot);
 
+                final Contact contact = dataSnapshot.getValue(Contact.class);
+                callNumber = contact.getPhone();
+
+                mFab1.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        if(ContextCompat.checkSelfPermission(mContext, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
+                            Log.d(TAG, "checkpermission");
+
+                            if(ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.CALL_PHONE)){
+                                Log.d(TAG, "shouldRequest");
+
+                                // Show an explanation to the user *asynchronously* -- don't block
+                                // this thread waiting for the user's response! After the user
+                                // sees the explanation, try again to request the permission.
+
+                                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+                                builder.setMessage("Vous devez autoriser l'application à utiliser votre appareil pour téléphoner");
+
+                                builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        requestPermissions(new String[]{Manifest.permission.CALL_PHONE}, CALL_PERMISSION);
+                                    }
+                                });
+                                builder.setNegativeButton("Annuler", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        dialog.dismiss();
+                                    }
+                                });
+
+                                AlertDialog dialog = builder.create();
+
+                                dialog.show();
+
+                            }else{
+
+                                // No explanation needed, we can request the permission.
+                                requestPermissions(new String[]{Manifest.permission.CALL_PHONE}, CALL_PERMISSION);
+                            }
+                        }else{
+                            Intent callIntent = new Intent(Intent.ACTION_CALL);
+                            callIntent.setData(Uri.parse("tel:" + callNumber));
+                            startActivity(callIntent);
+                        }
+
+
+                    }
+                });
             }
 
             @Override
@@ -110,6 +174,8 @@ public class ContactFragment extends Fragment {
 
             }
         });
+
+
 
         return v;
     }
@@ -137,6 +203,36 @@ public class ContactFragment extends Fragment {
             mFab1.setClickable(true);
             mFab2.setClickable(true);
             isFabOpen = true;
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull  String permissions[], @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        Log.d(TAG, "onRequestPermissionsResult");
+
+        switch (requestCode) {
+
+            case CALL_PERMISSION: {
+
+                Log.d(TAG, "CALL_PERMISSION");
+
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    Log.d(TAG, "permissionResult");
+
+                    Intent callIntent = new Intent(Intent.ACTION_CALL);
+                    callIntent.setData(Uri.parse("tel:" + callNumber));
+                    startActivity(callIntent);
+
+                }
+                return;
+            }
+
 
         }
     }
